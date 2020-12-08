c9edb8e1706bfd72f64c14793775494ea65d63f5
Bhanuka <bhanukamax@gmail.com>
2020-12-08 19:34:45 +0530

    Allow setting custom css classNames from options

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,12 @@
+type ClassNames = {
+  container?: string;
+  buttons?: string;
+  confirmButton?: string;
+  cancelButton?: string;
+};
+
 export type Options = {
-  labels?: {
-    confirmable: string;
-    cancellable: string;
-  };
+  labels?: { confirmable: string; cancellable: string };
+  classNames?: ClassNames;
   render?: (message: string, onConfirm: () => void, onCancel: () => void) => Element;
 };
