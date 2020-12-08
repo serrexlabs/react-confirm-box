05ef6db751692948abdb11f8494b0b82c8ac02a2
Bhanukamax <Bhanukamax@gmail.com>
2020-12-08 21:12:50 +0530

    Allow setting custom css classNames from options (#1)

--- src/confirm/index.tsx
+++ src/confirm/index.tsx
@@ -22,16 +22,39 @@ const ConfirmBox: React.FC<Props> = ({ resolver, message, options }: Props) => {
   };
 
   const render = () => {
+    const { classNames } = options || {};
+
+    const containerClassNames = `confirm - box__content $ {
+      classNames ?.container || ''
+    }
+    `;
+    const confirmButtonClassNames = `${classNames?.buttons || ''} $ {
+      classNames ?.confirmButton || ''
+    }
+    `;
+    const cancelButtonClassNames = `${classNames?.buttons || ''} $ {
+      classNames ?.cancelButton || ''
+    }
+    `;
+
     if (!options?.render) {
       return (
-        <div className="confirm-box__content">
+        <div className={containerClassNames}>
           <span>{message}</span>
           <div className="confirm-box__actions">
-            <button onClick={onConfirmPopup} role="confirmable-button">
+            <button
+              onClick={onConfirmPopup}
+              role="confirmable-button"
+              className={confirmButtonClassNames}
+            >
               {options?.labels?.confirmable ? options?.labels?.confirmable : 'Yes'}
             </button>
 
-            <button onClick={onCancelPopup} role="cancellable-button">
+            <button
+              onClick={onCancelPopup}
+              role="cancellable-button"
+              className={cancelButtonClassNames}
+            >
               {options?.labels?.cancellable ? options?.labels?.cancellable : 'No'}
             </button>
           </div>
